8811b749c8bda35e29039f22524be579ab8052f2
Svet <mursvyats@gmail.com>
2023-07-09 05:33:50 +0500

    Hover effects added

--- 05-05-23/sample.js
+++ 05-05-23/sample.js
@@ -5,3 +5,9 @@ function doMath(x, y) {
 const result = doMath(5, 87);
 
 console.log(result);
+
+const functionContainerVariable = function () {
+	return 20;
+}
+
+console.log(functionContainerVariable());
