a36a74c81f3fdfafd98994e4c5e127cf4697c53b
Svet <mursvyats@gmail.com>
2023-09-30 23:07:09 +0500

    apple-like "animation"

---
+++ fancy/apple-like/app.js
@@ -0,0 +1,37 @@
+const html = document.documentElement;
+const canvas = document.querySelector('.airpods-scrolling');
+const context = canvas.getContext('2d');
+const linkToImages = 'https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/';
+const frameCount = 147;
+
+const currentFrame = index => (
+    `${linkToImages}${index.toString().padStart(4, '0')}.jpg`
+);
+console.log(currentFrame(1));
+// Set canvas dimensions
+canvas.width=1158;
+canvas.height=770;
+const img = new Image();
+
+const updateImage = (index) => {
+    img.src = currentFrame(index);
+    context.drawImage(img, 0, 0);
+}
+
+const preloadImages = () => {
+    for (let i = 1; i < frameCount; ++i) {
+        const img = new Image();
+        img.src = currentFrame(i);
+    }
+};
+
+preloadImages();
+
+window.addEventListener('scroll', () => {
+    const scrollTop = html.scrollTop;
+    const maxScrollTop = html.scrollHeight - window.innerHeight;
+    const scrollFraction = scrollTop / maxScrollTop;
+    const frameIndex = Math.min(frameCount - 1, ~~(scrollFraction * frameCount) + 1)
+    console.log(frameIndex);
+    img.onload = updateImage(frameIndex);
+})
